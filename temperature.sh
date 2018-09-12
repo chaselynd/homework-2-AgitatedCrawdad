4b399bc443e58843ae74e2bd07ce1aeabc7342a7
chaselynd <chaselynd@ksu.edu>
2018-09-12 00:55:43 -0500

    Added Kelvin output to temperature.sh script

--- temperature.sh
+++ temperature.sh
@@ -4,5 +4,8 @@ echo "Temperature in Fahrenheit"
 echo $TempF
 
 TempC=$(echo "scale=10;($TempF-32)*(5/9)"|bc)
+TempK=$(echo "scale=10;($TempC+273)"|bc)        #line added by Chase Lynd
 echo "Temperature in Celsius"
 echo $TempC
+echo "Temperature in Kelvin"                    #line added by Chase Lynd
+echo $TempK                                     #line added by Chase Lynd
